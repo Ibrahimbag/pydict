82348e86b403005f609515d616b7b23a83c2b32c
İbrahim Bağ <ibrahimbag33@outlook.com>
2025-07-23 12:48:37 +0300

    Remove forgotten assign used for debugging purposes

--- os_language.py
+++ os_language.py
@@ -10,6 +10,5 @@ def get_os_language():
         language = locale.windows_locale[windll.GetUserDefaultUILanguage()]
         return language.split("_")[0]  # Return the language code without region
     elif os.name == "posix":
-        language = os.getenv('LANG').split("_")[0]
-        language = "tr"
+        language = os.getenv("LANG").split("_")[0]
         return language
