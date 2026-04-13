bd2c9886cede0e9787553c81b3638d3594e79923
İbrahim Bağ <ibrahimbag33@outlook.com>
2026-04-13 21:12:18 +0300

    Rename variable windll to kernel32 for clarity and remove unnecessary function call

--- os_language.py
+++ os_language.py
@@ -5,9 +5,8 @@ import ctypes
 
 def get_os_language():
     if os.name == "nt":
-        windll = ctypes.windll.kernel32
-        windll.GetUserDefaultUILanguage()
-        language = locale.windows_locale[windll.GetUserDefaultUILanguage()]
+        kernel32 = ctypes.windll.kernel32
+        language = locale.windows_locale[kernel32.GetUserDefaultUILanguage()]
         return language.split("_")[0]  # Return the language code without region
     elif os.name == "posix":
         language = os.getenv("LANG").split("_")[0]
